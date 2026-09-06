8fd7d621d5d091810f594dd2eeec332e034d5df8
Tushar Yadav <tusharforwork00@gmail.com>
2026-09-06 22:45:39 +0530

    Add verifyTools2.groovy to check required tools

---
+++ vars/verifyTools2.groovy
@@ -0,0 +1,15 @@
+def call() {
+
+    echo "Checking required tools"
+
+    sh '''
+        java -version
+        mvn -version
+        docker --version
+        aws --version
+        trivy --version
+        kustomize version
+    '''
+
+    echo "All required tools are available"
+}
