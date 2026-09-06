e492bf51f8f09fa30095f310208862b0465fab29
Tushar Yadav <tusharforwork00@gmail.com>
2026-09-06 21:07:45 +0530

    Modify mavenPackage to accept serviceDir parameter

--- vars/mavenPackage.groovy
+++ vars/mavenPackage.groovy
@@ -1,11 +1,12 @@
-def call (
+def call (serviceDir){
+
   echo "Packaging application"
 
-    dir('cart-service') {
+    dir(serviceDir) {
         sh '''
             mvn package -DskipTests
         '''
     }
 
     echo "Packaging completed successfully"
-)
+}
