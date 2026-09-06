056d31a709c41dc8195b48c2df766b14a18babc7
Tushar Yadav <tusharforwork00@gmail.com>
2026-09-06 21:33:14 +0530

    Add promoteToDev function for service promotion

---
+++ vars/promoteToDev.groovy
@@ -0,0 +1,53 @@
+def call(serviceName, gitopsRepo, gitopsBranch, imageName, imageTag, ecrRegistry) {
+
+    echo "Promoting ${serviceName} to Dev"
+
+    dir('k8s') {
+
+        deleteDir()
+
+        withCredentials([
+            string(
+                credentialsId: 'gitops-token',
+                variable: 'GITOPS_TOKEN'
+            )
+        ]) {
+
+            sh """
+                 git clone \
+                 --branch ${gitopsBranch} \
+                 https://x-access-token:${GITOPS_TOKEN}@github.com/${gitopsRepo}.git .
+            """
+
+            dir('overlays/dev') {
+
+                sh """
+                    kustomize edit set image \
+                    ${serviceName}=${ecrRegistry}/${imageName}:${imageTag}
+                """
+
+                sh '''
+                    kustomize build . > /tmp/dev-manifest.yaml
+                    test -s /tmp/dev-manifest.yaml
+                '''
+            }
+
+            sh '''
+                git config user.name "jenkins"
+                git config user.email "jenkins@yourcompany.com"
+
+                git add overlays/dev/kustomization.yaml
+
+                if git diff --cached --quiet; then
+                    echo "No GitOps changes required"
+                    exit 0
+                fi
+
+                git commit \
+                    -m "Promote ${serviceName} ${imageTag} to dev"
+
+                git push origin ${gitopsBranch}
+            '''
+        }
+    }
+}
